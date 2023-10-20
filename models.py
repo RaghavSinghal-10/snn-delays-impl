64881096771e9b50c4cdc7e837509b6661ce2dbd
Raghav Singhal <19d070049@iitb.ac.in>
2023-10-20 19:24:48 +0530

    added stuff

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 import snntorch as snn
 from snntorch import surrogate, utils
 from collections import OrderedDict
-from DCLS.construct.modules import Dcls1d
+from DCLS.construct.modules import *
     
 
 class SNN(nn.Module):
@@ -192,5 +192,171 @@ class SNN_Delay(nn.Module):
 
             for m in self.modules():
                 if isinstance(m, Dcls1d):
+                    m.P.round_()
+                    m.clamp_parameters()
+
+
+
+class SNN_Delay_Conv(nn.Module):
+
+    def __init__(self, beta, learn_beta, threshold, learn_threshold, time_steps, surr):
+
+        super().__init__()
+
+        if surr == 'atan':
+            self.spike_grad = surrogate.atan()
+        elif surr == 'fast_sigmoid':
+            self.spike_grad = surrogate.fast_sigmoid()
+
+        # initialize the parameters
+        self.learn_beta = learn_beta
+        self.learn_threshold = learn_threshold
+        self.beta = beta
+        self.threshold = threshold
+        self.time_steps = time_steps
+
+        # initialize the dcls parameters
+        self.left_pad = 250//self.time_steps
+        self.max_delay = 250//self.time_steps
+        self.max_delay = self.max_delay if self.max_delay%2==1 else self.max_delay+1
+        self.siginit = self.max_delay//2
+
+        # define the network
+        self.dcls3_1d_1 = Dcls3_1d(in_channels=2, out_channels=12, kernel_count=1, stride=1, 
+                                   padding=0, dense_kernel_size=self.max_delay, dense_kernel_size=(5,5))
+        self.mp_1 = nn.MaxPool2d(2)
+        self.ln_1 = nn.LayerNorm((12, 15, 15), elementwise_affine=False)
+        self.lif_1 = snn.Leaky(beta=self.beta, spike_grad=self.spike_grad, learn_beta=self.learn_beta, threshold=self.threshold, learn_threshold=self.learn_threshold)
+        self.dropout_1 = nn.Dropout(p=0.4)
+        self.dcls3_1d_2 = Dcls3_1d(in_channels=12, out_channels=32, kernel_count=1, stride=1,
+                                    padding=0, dense_kernel_size=self.max_delay, dense_kernel_size=(5,5))
+        self.mp_2 = nn.MaxPool2d(2)
+        self.ln_2 = nn.LayerNorm((32, 5, 5), elementwise_affine=False)
+        self.lif_2 = snn.Leaky(beta=self.beta, spike_grad=self.spike_grad, learn_beta=self.learn_beta, threshold=self.threshold, learn_threshold=self.learn_threshold)
+        self.dropout_2 = nn.Dropout(p=0.4)
+        self.flatten = nn.Flatten(start_dim=1, end_dim=3)
+        self.dcls1d_1 = Dcls1d(in_channels=32*5*5, out_channels=100, kernel_count=1, stride=1, padding=0, dilated_kernel_size=self.max_delay, groups=1, bias=True, padding_mode='zeros', version='gauss')
+        self.lif_3 = snn.Leaky(beta=self.beta, spike_grad=self.spike_grad, learn_beta=self.learn_beta, threshold=self.threshold, learn_threshold=self.learn_threshold)
+        self.dcls1d_2 = Dcls1d(in_channels=100, out_channels=10, kernel_count=1, stride=1, padding=0, dilated_kernel_size=self.max_delay, groups=1, bias=True, padding_mode='zeros', version='gauss')
+
+        # set P parameters of the dcls1d layers to random values between -max_delay//2 and max_delay//2
+        for m in self.modules():
+            if isinstance(m, Dcls1d) or isinstance(m, Dcls3_1d):
+                nn.init.uniform_(m.P, a=-self.max_delay//2, b=self.max_delay//2)
+                m.clamp_parameters()
+
+
+        # set sig parameters of the dcls layers to siginit and required_grad=False
+        for m in self.modules():
+            if isinstance(m, Dcls1d) or isinstance(m, Dcls3_1d):
+                print(m)
+                nn.init.constant_(m.SIG, self.siginit)
+                m.SIG.requires_grad = False
+
+
+    def forward(self, data):
+
+        # automate the code so as to pass through dcls2_1 to lif_3
+
+        mem_1 = self.lif_1.init_leaky()
+        mem_2 = self.lif_2.init_leaky()
+        mem_3 = self.lif_3.init_leaky()
+
+        # data [batch_size, time_steps, C, H, W] to [batch_size, C, H, W, time_steps]
+        data = data.permute(0,2,3,4,1)
+
+        # left pad the time_steps with T time steps of zeros
+        x_1 = F.pad(data, (self.left_pad,0), 'constant', 0)
+        #print(x_1.size())
+
+        x_1 = self.dcls3_1d_1(x_1)
+        #print(x_1.size())
+
+        spk_rec_1 = []
+
+        for step in range(x_1.size(4)):
+            in_1 = self.dropout_1(self.ln_1(self.mp_1(x_1[:,:,:,:,step])))
+            spk_out, mem_1 = self.lif_1(in_1, mem_1)
+            spk_rec_1.append(spk_out)
+
+        x_2 = torch.stack(spk_rec_1, dim=4)
+        #print(x_2.size())
+
+        x_2 = F.pad(x_2, (self.left_pad,0), 'constant', 0)
+        #print(x_2.size())
+
+        x_2 = self.dcls2_2(x_2)
+        #print(x_2.size())
+
+        spk_rec_2 = []
+
+        for step in range(x_2.size(4)):
+            in_2 = self.dropout_2(self.ln_2(self.mp_2(x_2[:,:,:,:,step])))
+            spk_out, mem_2 = self.lif_2(in_2, mem_2)
+            spk_rec_2.append(spk_out)
+
+        x_3 = torch.stack(spk_rec_2, dim=4)
+        #print(x_3.size())
+
+        x_3 = F.pad(x_3, (self.left_pad,0), 'constant', 0)
+
+        # now change x_3 from [batch_size, C, H, W, time_steps] to [batch_size, C*H*W, time_steps]
+
+        x_3 = self.flatten(x_3)
+
+        x_3 = self.dcls1_1(x_3)
+        #print(x_3.size())
+
+        spk_rec_3 = []
+        mem_rec_3 = []
+
+        for step in range(x_3.size(2)):
+            spk_out, mem_3 = self.lif_3(x_3[:,:,step], mem_3)
+            spk_rec_3.append(spk_out)
+            mem_rec_3.append(mem_3)
+
+        #print(torch.stack(spk_rec_3).size())
+
+        #output of the form [time_steps, batch_size, features]
+        return torch.stack(spk_rec_3), torch.stack(mem_rec_3)
+
+
+    def reset_model(self, train=True):
+
+        # clamp parameters of dcls layers
+
+        for m in self.modules():
+            if isinstance(m, Dcls1d) or isinstance(m, Dcls3_1d):
+                m.clamp_parameters()
+
+
+    def decrease_sig(self, epoch, num_epochs, time_steps):
+
+        # Decreasing to 0.23
+        final_epoch = num_epochs//4
+        alpha = 0
+
+        max_delay = 250//time_steps
+        max_delay = max_delay if max_delay%2==1 else max_delay+1
+
+        siginit = max_delay//2
+
+        # take sig from last dcls layer  
+        sig = self.dcls1_1.SIG[0,0,0,0].detach().cpu().item()
+
+        if epoch < final_epoch and sig > 0.23:
+
+            alpha = (0.23/siginit)**(1/(final_epoch))
+
+            for m in self.modules():
+                if isinstance(m, Dcls1d) or isinstance(m, Dcls3_1d):
+                    m.SIG *= alpha
+
+
+    def round_pos(self):
+        with torch.no_grad():
+
+            for m in self.modules():
+                if isinstance(m, Dcls1d) or isinstance(m, Dcls3_1d):
                     m.P.round_()
                     m.clamp_parameters()
---
+++ train-conv.py
@@ -0,0 +1,189 @@
+import torch
+import snntorch as snn
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from snntorch import surrogate
+from snntorch import functional as SF
+from snntorch import spikeplot as splt
+from snntorch import utils
+import tonic
+import argparse
+import wandb
+from models import *
+
+
+wandb.init(project='snn delay with conv')
+
+parser = argparse.ArgumentParser(description='Evaluate SNN with delay conv')
+
+parser.add_argument('--batch_size_train', default=256, type=int, help='batch size for training the linear classifier')
+parser.add_argument('--batch_size_test', default=256, type=int, help='batch size for training the linear classifier')
+parser.add_argument('--num_epochs', default=200, type=int, help='number of epochs for training the linear classifier')
+parser.add_argument('--num_workers', default=8, type=int, help='number of workers for loading the data')
+parser.add_argument('--lr', default=0.005, type=float, help='learning rate for training the linear classifier')
+parser.add_argument('--momentum', default=0.9, type=float, help='momentum for training the linear classifier')
+parser.add_argument('--device', default='cuda', type=str, help='device to be used for training the linear classifier')
+parser.add_argument('--learn_beta', action='store_true', help='learn beta for the LIF neurons in the backbone')
+parser.add_argument('--learn_threshold', action='store_true', help='learn threshold for the LIF neurons in the backbone')
+parser.add_argument('--threshold', default=1.0, type=float, help='threshold for the LIF neurons in the linear classifier')
+parser.add_argument("--surr", default="atan", type=str, help="surrogate neuron: atan, fast_sigmoid")
+
+parser.add_argument('--label_percent', default=0.1, type=float, help='percentage of labeled data to be used for training the linear classifier')
+parser.add_argument('--beta', default=0.5, type=float, help='beta for the LIF neurons in the backbone')
+parser.add_argument('--time_steps', default=30, type=int, help='number of time steps for the SNN')
+
+args = parser.parse_args()
+
+wandb.config.update(args)
+
+
+def main():
+
+    print(args)
+
+    device = args.device if torch.cuda.is_available() else "cpu"
+
+    model = SNN_Delay(beta=args.beta_bb, threshold=args.threshold_bb, surr = args.surr, final_channels=args.size_in,
+                            learn_beta=args.learn_beta_bb, learn_threshold=args.learn_threshold_bb, time_steps=args.time_steps).to(device)
+
+    #print trainable parameters of the model
+    print("Parameters of the bb model: ")
+    for name, param in model.named_parameters():
+        if param.requires_grad:
+            print(name, param.data.shape)
+
+    print(model)
+    
+    sensor_size = tonic.datasets.NMNIST.sensor_size
+
+    transform = tonic.transforms.Compose([
+            tonic.transforms.Denoise(filter_time=10000),
+            tonic.transforms.ToFrame(sensor_size=sensor_size, n_time_bins=args.time_steps)
+            ])
+
+    full_dataset = tonic.datasets.NMNIST(save_to='./data', train=True, transform=transform)
+    testset = tonic.datasets.NMNIST(save_to='./data', train=False, transform=transform)
+
+    if args.label_percent == 100:
+        trainset = full_dataset
+    else:
+        # use a random 10% of the labeled data
+        trainset, _ = torch.utils.data.random_split(full_dataset, [int(len(full_dataset)*args.label_percent/100), len(full_dataset)-int(len(full_dataset)*args.label_percent/100)])    
+    
+    print("trainset: ", len(trainset))
+    print("testset: ", len(testset))
+
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size_train, shuffle=True, num_workers=args.num_workers, drop_last=False)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size_test, shuffle=False, num_workers=args.num_workers, drop_last=False)
+
+    criterion = SF.mse_count_loss(correct_rate=0.8, incorrect_rate=0.2)
+
+    p_params = []
+    w_params = []
+
+    for name, param in model.named_parameters():
+        if param.requires_grad and '.P' in name:     
+            p_params.append(param)
+            print("p params", name)
+        elif param.requires_grad:
+            w_params.append(param)
+            print("w params", name)
+
+    optimizer_p = torch.optim.Adam(model.parameters(), lr=args.lr, betas=(0.9, 0.999))
+    optimizer_w = torch.optim.Adam(model.parameters(), lr=args.lr, betas=(0.9, 0.999))
+
+
+    loss_history = []
+    accuracy_history = []
+
+    model.train()
+
+    for epoch in range(args.num_epochs):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+
+        for i, (input, labels) in enumerate(trainloader, 0):
+            
+            labels = labels.type(torch.LongTensor)
+
+            input = input.to(device)
+            labels = labels.to(device)
+
+            input = input.type(torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor)
+
+            optimizer_p.zero_grad()
+            optimizer_w.zero_grad()
+
+            #output of the form [time_steps, batch_size, features]
+            spk_rec, mem_rec = model(input)
+
+            loss = criterion(spk_rec, labels)
+            loss.backward()
+
+            optimizer_w.step()
+            optimizer_p.step()
+
+            model.reset_model(train=True)
+
+            # print statistics
+            running_loss += loss.detach().item()
+
+            wandb.log({"train_loss": loss.detach().item()})
+
+        #print epoch loss
+        loss_history.append(running_loss / len(trainloader))
+        print('[%d] loss: %.3f' % (epoch + 1, running_loss / len(trainloader)))
+
+        total = 0
+        running_acc = 0.0
+
+        model.decrease_sig(epoch=i, num_epochs=args.num_epochs, time_steps=args.time_steps)
+
+
+        model.eval()
+
+        with torch.no_grad():
+
+            for m in model.modules():
+                if isinstance(m, Dcls1d) or isinstance(m, Dcls3_1d):
+                    m.SIG *= 0
+                    m.version = 'max'
+                    m.DCK.version = 'max'
+
+            model.round_pos()
+
+            for i, (images, labels) in enumerate(testloader, 0):
+                
+                labels = labels.type(torch.LongTensor)
+
+                images = images.to(device)
+                labels = labels.to(device)
+
+                images = images.type(torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor)
+                #labels = labels.type(torch.cuda.LongTensor if torch.cuda.is_available() else torch.FloatTensor)    
+
+                spk_rec, mem_rec = model(images)
+
+                acc = SF.accuracy_rate(spk_rec, labels)*100
+
+                model.reset_model(train=False)
+
+                total += labels.size(0)
+                running_acc += acc
+
+            for m in model.modules():
+                if isinstance(m, Dcls1d) or isinstance(m, Dcls3_1d):
+                    m.version = 'gauss'
+                    m.DCK.version = 'gauss'
+
+            accuracy = running_acc / len(testloader)
+            accuracy_history.append(accuracy)
+            wandb.log({"test_acc": accuracy})
+
+            print("Total: {}, Accuracy: {}".format(total, accuracy))
+          
+if __name__ == '__main__':
+    main()
